1eafa6738dda6b64c61e5c1d5bdd81be97a6706c
Deepak Sankhyan <100864803+gitdeepaks@users.noreply.github.com>
2023-06-04 18:51:01 +0000

    adding env

--- src/models/collection.schema.js
+++ src/models/collection.schema.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 
-const collectionScema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: ["true", "Please provide a product or a collection name"],
-    trim: true,
-    maxLength: [120, "Collection name should not be more than 120 chars"],
+const collectionScema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: ["true", "Please provide a product or a collection name"],
+      trim: true,
+      maxLength: [120, "Collection name should not be more than 120 chars"],
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Collection", collectionScema);
