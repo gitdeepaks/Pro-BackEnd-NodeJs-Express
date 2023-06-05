ecca9c309810ff6c84121656b1cb348bec306bb8
Deepak Sankhyan <100864803+gitdeepaks@users.noreply.github.com>
2023-06-05 15:19:19 +0000

    product schema

---
+++ src/models/product.schema.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: ["true", "please provide product name"],
+      trim: true,
+      maxLength: [120, "product name should not be max than 120 chars"],
+    },
+    price: {
+      type: Number,
+      required: ["true", "please provide product price"],
+      maxLength: [5, "product name should not be max than 5 chars"],
+    },
+    description: {
+      type: String,
+    },
+    photos: [
+      {
+        secure_url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    collectionId: {
+      ref: "Collection",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Product", productSchema);
