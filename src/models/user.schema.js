f5d3162687e4d470d8159956235293d6d3f16c51
Deepak Sankhyan <100864803+gitdeepaks@users.noreply.github.com>
2023-06-05 13:15:07 +0000

    hooks and methide in mongoose

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles.js";
+import bycrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -28,5 +29,20 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+//Encript the Password before saving: HOOKS
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bycrypt.hash(this.password, 10); //10 is a salt value
+  next();
+});
+
+userSchema.methods = {
+  //compare password
+  comparePassword: async function (enteredPassword) {
+    return await bycrypt.compare(enteredPassword, this.password);
+  },
+};
 
 export default mongoose.model("User", userSchema);
