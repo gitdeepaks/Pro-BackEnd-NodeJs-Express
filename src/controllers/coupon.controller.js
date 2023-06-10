ca09f03071ac4cacbfb9fb49017d4b6b79a3edd6
Deepak Sankhyan <100864803+gitdeepaks@users.noreply.github.com>
2023-06-10 15:40:24 +0000

    added coupon controller

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -1,4 +1,5 @@
-import Coupon from "../models/coupon.schema.js"
+import Coupon from "../models/coupon.schema.js";
 import asyncHandler from "../service/asyncHandler.js";
+import CustomError from "../utils/CustomError.js";
 
 // src/models/coupon.schema.js src/service/asyncHandler.js
