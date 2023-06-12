092c016617e58a18aea4f1681bcc1393e58c1202
Deepak Sankhyan <100864803+gitdeepaks@users.noreply.github.com>
2023-06-12 14:27:14 +0000

    added razorpay

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -3,3 +3,6 @@ import Coupon from "../models/coupon.schema.js";
 import Order from "../models/order.schema.js";
 import asyncHandler from "../service/asyncHandler.js";
 import CustomError from "../utils/CustomError.js";
+import razorpay from "../config/razorpay.config.js";
+
+export const generateRazorpayOrderId = asyncHandler(async (res, req) => {});
