20564b7a5bab5bf8a7fb5c9d5db0b2ea4e9bc947
Deepak Sankhyan <100864803+gitdeepaks@users.noreply.github.com>
2023-06-13 10:48:18 +0000

    added some more controllers

--- src/controllers/coupon.controller.js
+++ src/controllers/coupon.controller.js
@@ -2,7 +2,6 @@ import Coupon from "../models/coupon.schema.js";
 import asyncHandler from "../service/asyncHandler.js";
 import CustomError from "../utils/CustomError.js";
 
-// src/models/coupon.schema.js src/service/asyncHandler.js
 export const createCoupon = asyncHandler(async (req, res) => {
   const { code, discount } = res.body;
 
